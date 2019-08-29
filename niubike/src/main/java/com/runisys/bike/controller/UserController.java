3961b02cdc3698286099c6987699be7ee9ff5ada
xrw560 <ncutits@163.com>
2019-08-29 21:38:30 +0800

    nginx负载均衡

--- niubike/src/main/java/com/runisys/bike/controller/UserController.java
+++ niubike/src/main/java/com/runisys/bike/controller/UserController.java
@@ -2,6 +2,10 @@ package com.runisys.bike.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 public class UserController {
@@ -9,6 +13,19 @@ public class UserController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/host")
+    @ResponseBody
+    public String host(){
+        String host = null;
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+            System.out.println(InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return host;
     }
 
 }
